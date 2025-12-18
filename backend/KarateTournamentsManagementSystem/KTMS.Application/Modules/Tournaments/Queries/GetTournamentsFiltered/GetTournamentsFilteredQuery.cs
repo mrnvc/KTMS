7416df6202c9e370af02a9b784bcf61f492c42dc
lamija <lamija.hadzimehmedagic@edu.fit.ba>
2025-12-18 18:22:43 +0100

    Tournaments filtering

---
+++ backend/KarateTournamentsManagementSystem/KTMS.Application/Modules/Tournaments/Queries/GetTournamentsFiltered/GetTournamentsFilteredQuery.cs
@@ -0,0 +1,25 @@
+﻿using KTMS.Application.Modules.Tournaments.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTMS.Application.Modules.Tournaments.Queries.GetTournamentsFiltered
+{
+   public class GetTournamentsFilteredQuery : IRequest<List<TournamentsDto>>
+    {
+        public string? Title {get; set;}
+        public string? City { get; set;}
+        public string? Country { get; set;}
+        public string? Adress { get; set;}
+        public DateOnly? Date { get; set;}
+        public int? Year { get; set;}
+        public int? Month { get; set;}
+        public int? Day { get; set;}
+
+
+
+    }
+}
