fb354f414aad968b487f7c7af621ee8d29a8fcdb
sara <sara.muranovic@edu.fit.ba>
2025-12-25 20:10:45 +0100

    frontend started from scratch

--- frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import {LandingComponent} from './landing/landing';
-
-export const routes: Routes = [
-  {path: '', component: LandingComponent}
-];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,15 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: false,
+  styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('ktms_landingPAge');
+  protected readonly title = signal('frontend');
 }
