6b4d70f4cf8bc816f7825f9631a9b6a5b7dacef9
sara <sara.muranovic@edu.fit.ba>
2026-02-27 14:06:59 +0100

    modifications

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import {Component, computed, OnInit, signal} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 
 @Component({
